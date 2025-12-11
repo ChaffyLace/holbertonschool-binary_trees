f49f2696f8a50aa346e990394264a48fe947b6c2
ChaffyLace <132900529+ChaffyLace@users.noreply.github.com>
2025-12-11 10:17:15 +0100

    betty task 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,36 +1,35 @@
 #include "binary_trees.h"
-#include <stdlib.h>
 
 /**
- * binary_tree_insert_left - Insère un nœud comme enfant gauche du parent.
+ * binary_tree_insert_left - Inserts a node as the left child of another node
+ * @parent: Pointer to the parent node
+ * @value: Value to store in the new node
  *
- * @parent: Pointeur vers le nœud où insérer l'enfant gauche.
- * @value: La valeur à stocker dans le nouveau nœud.
- *
- * Si parent a déjà un enfant gauche, le nouveau nœud prend sa place,
- * et l'ancien enfant gauche devient l'enfant gauche du nouveau nœud.
- *
- * Return: Pointeur vers le nœud créé, ou NULL si parent est NULL ou en cas d'échec.
+ * Return: Pointer to the new node or NULL on failure
  */
-binary_tree_t *binary_tree_insert_left(
-		binary_tree_t *parent, int value)
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node;
+binary_tree_t *new_node;
 
-	if (parent == NULL)
-		return (NULL);
+if (!parent)
+return (NULL);
 
-	new_node = binary_tree_node(parent, value);
-	if (new_node == NULL)
-		return (NULL);
+if (parent->left)
+{
+new_node = binary_tree_node(parent, value);
+if (!new_node)
+return (NULL);
+new_node->left = parent->left;
+parent->left->parent = new_node;
+parent->left = new_node;
+}
+else
+{
+parent->left = binary_tree_node(parent, value);
+if (!parent->left)
+return (NULL);
+}
 
-	if (parent->left != NULL)
-	{
-		new_node->left = parent->left;
-		new_node->left->parent = new_node;
-	}
-
-	parent->left = new_node;
-
-	return (new_node);
+return (parent->left);
 }
